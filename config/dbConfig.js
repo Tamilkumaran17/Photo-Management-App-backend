a59ab12d657c6473d8da75d821d0df749c02e807
tamilkumaran <tamilkumaran1494@gmail.com>
2025-03-27 12:13:42 +0530

    backend connected

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-const connection = mongoose.createConnection('mongodb+srv://tamilkumaran1494:Tamil2004@cluster0.o3hc8rf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').on('open',()=>{
+
+// mongodb+srv://tamilkumaran1494:Tamil2004@cluster0.o3hc8rf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+const connection = mongoose.createConnection('mongodb+srv://tamilkumaran1494:tamil2004@cluster0.o3hc8rf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').on('open',()=>{
     console.log("MongoDB connected");
 }).on("error",(error)=>{
     console.error(error.message);
